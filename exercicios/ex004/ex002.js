65888b5450c13fbe35089b6a2b85bbdc40a5fbc0
Laylateles <135770455+Laylateles@users.noreply.github.com>
2023-07-27 21:12:46 -0300

    exercicios detran

---
+++ exercicios/ex004/ex002.js
@@ -0,0 +1,9 @@
+var país = 'EUA'
+console.log(`Vivendo em ${país} você é` )
+if (país == 'Brasil'){
+    console.log('Brasileiro')
+} else {
+    console.log('Estrangeiro')
+}
+
+// condição composta
