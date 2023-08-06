488d496500d8ea053f25f11268664e4ea25c3e12
Laylateles <135770455+Laylateles@users.noreply.github.com>
2023-08-06 19:51:03 -0300

    repetição

---
+++ exercicios/ex008/rep.js
@@ -0,0 +1,34 @@
+var a = 1
+do {
+    console.log(`Passo ${a}`)
+    a++
+} while(a <= 6)
+// basicamente tem o mesmo resultado q o outro,vai ter situações q é melhor usar um doq o outro
+//do = faça 
+
+
+
+//  var a = 1
+//  while(a <= 6){
+//      console.log(`Passo ${a}`)
+//      a++ //a++ = a = a+1
+//  } 
+//  quando usa-se o a++ ele vai até infinito,muito usável!!! Não se esquecer nunca
+
+
+
+
+// var a = 1
+// while(a <= 6){
+//     console.log('Teste')
+//     a++ //a++ = a = a+1 quando chegar aq embaixo ele vai valer 2 ai quando voltar ele vai valer 3 e sucessivamente até o número entre parêntese
+// repetição usando while=quando }
+
+
+// repetição
+// console.log('Teste')
+// console.log('Teste')
+// console.log('Teste')
+// console.log('Teste')
+// console.log('Teste')
+// console.log('Teste')
