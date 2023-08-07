5024fcebaf22f6f763d6e4ec88c92134505e2109
Laylateles <135770455+Laylateles@users.noreply.github.com>
2023-08-07 20:44:55 -0300

    exercicio dia,noite

--- estudos/noite,dia e tarde/proji.js
+++ estudos/noite,dia e tarde/proji.js
@@ -2,7 +2,20 @@ function carregar(){
     var data = new Date()
     var hora = data.getHours()
     var mensagem = window.document.getElementById('mensa')
-    var img = window.document.createElement('img')
-    img.setAttribute('id','foto')
+    var nome = window.prompt('Digite seu nome por favor:')
+    var img = window.document.getElementById('imagem')
     mensagem.innerHTML= `Olá seu horário é ${hora} horas.`
+    if (hora < 12) {
+        img.src = 'dia.jpeg'
+        document.body.style.backgroundColor ='#ddb441'
+        mensagem.innerHTML += `<p>Olá ${nome}! Bom dia!</p>`
+    } else if(hora < 18){
+        img.src = 'tarde.jpeg'
+        document.body.style.backgroundColor ='#e79b28'
+        mensagem.innerHTML += `<p>Olá ${nome}! Boa Tarde!</p>`
+    } else {
+        img.src = 'noite.jpeg'
+        document.body.style.backgroundColor ='#585454'
+        mensagem.innerHTML += `<p>Olá ${nome}! Boa Noite!</p>`
+    }
 }
