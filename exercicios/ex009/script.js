74e9196c32fbaec6b25ef312efd3a4e30d2c2dbc
Laylateles <135770455+Laylateles@users.noreply.github.com>
2023-08-12 21:46:51 -0300

    primeiro exercicio feito

--- exercicios/ex009/script.js
+++ exercicios/ex009/script.js
@@ -3,9 +3,42 @@ function clicar(){
     var fim = window.document.getElementById('fim')
     var passo = window.document.getElementById('pas')
     var resu = window.document.getElementById('res')
-    if (inicio.value.length == fim.value.length) {
-        resu.innerHTML = 'Não foi possível contar,pois os números são iguais!'
+    if(inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        resu.innerHTML = `Impossível contar!`
     } else {
-    resu.innerHTML = `Contando de ${inicio} até ${fim}`
+        resu.innerHTML = `Contando:`
+        var i =Number(inicio.value)
+        var f =Number(fim.value)
+        var p =Number(passo.value)
+
+        //  if(p = 0){
+        //      window.alert('Passo inválido,considerando PASSO 1!')
+        //      p = 1 ------- por algum motivo o google trava quando coloco pra rodar
+        //  }
+          if (i < f) {
+        //contagem crescente
+        for(var c = i ; c <= f ; c += p){
+            resu.innerHTML += ` ${c} `
+        }
+       } else {
+        // contagem regressiva
+        for(var c = i ; c >= f ; c -= p)
+        resu.innerHTML += ` ${c} `
+       }
     }
+    
 }
+
+/* o primerio else: o number está convertendo os dados digitados no input para poder realizar a soma.
+o segundo if é para se o valor inicial digitado for menor q o fim,assim a var contador se inicia no i(var c = i),enquanto o contador for menor q f (c <= f),o valor do contador somado ao passo(c += p)
+o mesmo esquema no else mas enves d somar o passo vc diminui(c -= p)
+
+
+
+
+
+
+
+
+
+*/
