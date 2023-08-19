27fa9e651cdf73832b402d7731d8760b6d150b42
Laylateles <135770455+Laylateles@users.noreply.github.com>
2023-08-19 16:22:33 -0300

    refazendo desafios passados

--- desafio/des008/script.js
+++ desafio/des008/script.js
@@ -1,9 +1,17 @@
 function calcular() {
-   var prod = window.prompt('Qual o produto que você está comprando?')
-   var preço = window.prompt(`Qual é o preço de ${prod}?`)
-   var porcent =Number(preço.value)
-   var div = porcent / 10
-   var sub = porcent - div
+   var prod =window.prompt('Qual o produto que você está comprando?')
+   var preço =Number (window.prompt(`Qual é o preço de ${prod}?`))
+   var desc = preço * 0.1
+   var sub = preço - desc
    var res = window.document.getElementById('res')
-   res.innerHTML = `O valor do desconto foi de ${div}`
+   res.innerHTML  = `<h1>Calculando desconto de 10% para ${prod}</h1>`
+   res.innerHTML += `O preço original era de R$${preço.toFixed(2)}`
+   res.innerHTML += `<p>O valor do desconto foi de R$${desc.toFixed(2)}</p>`
+   res.innerHTML += `<p>Você irá pagar R$${sub.toFixed(2)}</p>`
 }
+
+
+//CONSEGUI FAZER SOZINHA AAAAAAAAAAA GLÓRIA (19/08/2023)
+
+
+//R$${desc.toFixed(2)}---dentro do ${}
