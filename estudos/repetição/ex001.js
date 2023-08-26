1d6e5229b26d1efbb0beabffe87e9049af08263d
Laylateles <135770455+Laylateles@users.noreply.github.com>
2023-08-25 21:04:17 -0300

    fazendo mais exercicios

---
+++ estudos/repetição/ex001.js
@@ -0,0 +1,17 @@
+var a = 1 // inicialização
+while (a < 10){//teste lógico
+    console.log(`O número foi ${a}`)
+    a++ // encremento
+}
+//while=enquanto,do=faça,
+
+var b = 1 //inicialização
+do{ // faça
+    console.log(`Contando ${b}`)
+    b++ // encremento
+} while(b < 10) // teste lógico no final
+
+//inicializa,teste,encremento
+for(var c = 1;c < 10;c++){
+    console.log(`Testando ${c}`)
+}
---
+++ estudos/repetição/ex002.js
@@ -0,0 +1,21 @@
+for(var a = 1; a <= 10 ;a++){
+    console.log(`Fazendo testes,número:${a}`)
+}
+
+var i = 1
+for(var c = i;c < 10 ;c++){
+    console.log(`O número ${c},é um bom número!`)
+}
+//quando se é criada outra var,usa-se ela para os incrementos e teste lógico
+
+var b = 1
+var c = 0
+if(c > b){
+    for(var d = b;d < 10;d++){
+        console.log(`O número ${b} é menor do que ${d}.`)
+    }
+} else { // b > c 
+    for(var a = b; a < 10; a++) {
+        console.log(`O número ${c} é menor que ${a}`)
+    }
+}
