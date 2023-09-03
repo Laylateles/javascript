bfae7b3ef63037f45146ae805b168a097261a067
Laylateles <135770455+Laylateles@users.noreply.github.com>
2023-09-03 18:05:51 -0300

    seguimos

--- exercicios/ex013/ex001/script.js
+++ exercicios/ex013/ex001/script.js
@@ -1,15 +1,23 @@
 function adicionar(){
-    var n = [Number(window.document.getElementById('n').value)]
+    var n = Number(window.document.getElementById('n').value)
+    var num = [n]
     var res = window.document.getElementById('res')
-    if(n == 0 || n > 100){
+    if( num <= 0 || num > 100){
         window.alert('[ERRO] digite um número válido!')
     } else {
-    for(var c = n - 1 ; c < n ;c++){
-        res.innerHTML += `<p>Valor ${n} adicionado</p>`
+    for(var c = 0 ; c < num.length;c++){
+        res.innerHTML += `<p>Valor ${num} adicionado</p>`
     }
-    } 
-
+    }
 } 
+
+/* function finalizar(){
+    var n = Number(window.document.getElementById('n').value)
+    var num = [n]
+    var res = window.document.getElementById('res')
+    res.innerHTML += `Ao todo,temos ${num.length} números cadastrados`
+} */
+
 
 
 
