88f47666470a5ac5a502d423ba8e822bafd66dee
Laylateles <135770455+Laylateles@users.noreply.github.com>
2023-09-04 21:11:55 -0300

    arrumando o código

--- exercicios/ex013/ex001/script.js
+++ exercicios/ex013/ex001/script.js
@@ -1,22 +1,34 @@
-function adicionar(){
-    var n = Number(window.document.getElementById('n').value)
-    var num = [n]
-    var res = window.document.getElementById('res')
-    if( num <= 0 || num > 100){
-        window.alert('[ERRO] digite um número válido!')
-    } else {
-    for(var c = 0 ; c < num.length;c++){
-        res.innerHTML += `<p>Valor ${num} adicionado</p>`
+function isNumero(n){
+    if(Number(n) >= 1 && Number(n) <= 100){
+        return true
+    } else{
+        return false
     }
+}
+function inLista(n , l){
+    if(l.indexOf(Number(n)) != -1){
+        return true
+    } else{
+        return false
+    }
+}
+
+function adicionar(){
+    var num= Number(window.document.getElementById('n').value)
+    var valores = []
+    var res = window.document.getElementById('res')
+    if(isNumero(num) && !inLista(num , valores)){
+        window.alert('Tudo ok!')
+    } else {
+        window.alert('[ERRO] digite um número válido!')
     }
 } 
-
-/* function finalizar(){
-    var n = Number(window.document.getElementById('n').value)
-    var num = [n]
-    var res = window.document.getElementById('res')
-    res.innerHTML += `Ao todo,temos ${num.length} números cadastrados`
-} */
+/*for(var c = 0; c < num.length;c++){
+    res.innerHTML += `<p>Valor ${num} adicionado</p>`
+}
+function finalizar(){
+   // res.innerHTML += `Ao todo,temos ${num.length} números cadastrados`
+}*/
 
 
 
