73b3e7afa805f351ec6b14b66a283e8425237b93
Laylateles <135770455+Laylateles@users.noreply.github.com>
2023-09-06 21:21:31 -0300

    finalizando

--- exercicios/ex013/ex001/script.js
+++ exercicios/ex013/ex001/script.js
@@ -1,10 +1,9 @@
-var num =window.document.getElementById('n')
-var teste = Number(num.value)
-var lista = window.document.getElementById('list')
-var res = window.document.getElementById('res')
+var num =document.querySelector('input#n')
+var lista = document.querySelector('select#list')
+var res = document.querySelector('div#res')
 var valores = []
 
-function isNumero(n){
+function isNumero(n) {
     if(Number(n) >= 1 && Number(n) <= 100){
         return true
     } else{
@@ -21,20 +20,50 @@ function inLista(n, l){
 }
 
 function adicionar() {
-    if(isNumero(teste) && !inLista(teste, valores)) { 
-        valores.push(teste)
+    var num =document.querySelector('input#n')
+    var lista = document.querySelector('select#list')
+    var res = document.querySelector('div#res')
+    if(isNumero(num.value) && !inLista(num.value, valores)) { 
+       valores.push(Number(num.value))
+       var item = document.createElement('option')
+       item.text = `Valor ${num.value} adicionado`
+       lista.appendChild(item)
+       res.innerHTML = ''
     } else {
         window.alert('Valor inválido ou já encontrado na lista!')
     }
+    num.value = ''
+    num.focus()
 } 
 
-
-/*for(var c = 0; c < num.length;c++){
-    res.innerHTML += `<p>Valor ${num} adicionado</p>`
-}
 function finalizar(){
-   // res.innerHTML += `Ao todo,temos ${num.length} números cadastrados`
-}*/
+    var res = document.querySelector('div#res')
+   if(valores.length == 0){
+    window.alert('Digite algum valor antes de finalizar!')
+   } else{
+    var total = valores.length//pra saber a quantidade d números
+    var maior = valores[0]
+    var menor = valores[0]
+    var soma = 0
+    var media = 0
+    for(var pos in valores){
+        soma += valores[pos]
+        if(valores[pos] > maior){
+            maior = valores[pos]
+        }
+        if(valores[pos] < menor){
+            menor = valores[pos]
+        }
+    }
+    media = soma / valores.length//consegui sozinha!!
+    res.innerHTML = '' // zerar o valor
+    res.innerHTML += `<p>Ao todo foram <strong>${total}</strong> números cadastrados</p>`
+    res.innerHTML += `<p> O maior valor informado foi <strong>${maior}</strong> </p>`
+    res.innerHTML += `<p>O menor valor informado foi <strong>${menor} </strong></p>`
+    res.innerHTML += `<p>A soma dos números cadastrados foi <strong>${soma}</strong> </p>`
+    res.innerHTML += `<p>A média dos número é <strong>${media.toFixed(2)}</strong></p>`
+   }
+}
 
 
 
--- exercicios/ex013/resolucao do exercicio com comentario/script.js
+++ exercicios/ex013/resolucao do exercicio com comentario/script.js
@@ -31,7 +31,51 @@ if(l.indexOf(Number(n)) != -1){
 /* se a posição do número digitado no array for diferente de -1 então retorna verdadeiro se não se ele tiver a posição -1 retorna falso
 */
 
+function adicionar() {
+    var num =document.querySelector('input#n')
+    var lista = document.querySelector('select#list')
+    var res = document.querySelector('div#res')
+    if(isNumero(num.value) && !inLista(num.value, valores)) { 
+       valores.push(Number(num.value))
+       var item = document.createElement('option')
+       item.text = `Valor ${num.value} adicionado`
+       lista.appendChild(item)
+       res.innerHTML = ''
+    } else {
+        window.alert('Valor inválido ou já encontrado na lista!')
+    }
+    num.value = ''
+    num.focus()
+} 
 
+function finalizar(){
+    var res = document.querySelector('div#res')
+   if(valores.length == 0){
+    window.alert('Digite algum valor antes de finalizar!')
+   } else{
+    var total = valores.length//pra saber a quantidade d números
+    var maior = valores[0]
+    var menor = valores[0]
+    var soma = 0
+    var media = 0
+    for(var pos in valores){
+        soma += valores[pos]
+        if(valores[pos] > maior){
+            maior = valores[pos]
+        }
+        if(valores[pos] < menor){
+            menor = valores[pos]
+        }
+    }
+    media = soma / valores.length//consegui sozinha!!
+    res.innerHTML = '' // zerar o valor
+    res.innerHTML += `<p>Ao todo foram <strong>${total}</strong> números cadastrados</p>`
+    res.innerHTML += `<p> O maior valor informado foi <strong>${maior}</strong> </p>`
+    res.innerHTML += `<p>O menor valor informado foi <strong>${menor} </strong></p>`
+    res.innerHTML += `<p>A soma dos números cadastrados foi <strong>${soma}</strong> </p>`
+    res.innerHTML += `<p>A média dos número é <strong>${media.toFixed(2)}</strong></p>`
+   }
+}
 
 
 
