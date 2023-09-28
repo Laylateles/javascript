23213db5d9b43f06ea550a10d74ab63ee76d6b6b
Laylateles <135770455+Laylateles@users.noreply.github.com>
2023-09-28 12:50:03 -0300

    estudando

---
+++ estudos/traversy/ex001/script.js
@@ -0,0 +1,5 @@
+var nasci = Number(prompt('Qual o ano que você nasceu?'))
+var ano = 2023
+var Idade = ano - nasci
+window.alert(`Você têm ${Idade} anos! \nComo você é jovem!`) 
+//  o \n é o quebra de linha do js
