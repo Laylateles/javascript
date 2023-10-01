e1a4474c4df6bc6bbf126768fffdceba1b4644dc
Laylateles <135770455+Laylateles@users.noreply.github.com>
2023-10-01 09:59:44 -0300

    create

---
+++ estudos/traversy/ex002/01.js
@@ -0,0 +1,17 @@
+// o que aprendi até agr em js:n
+var n = 0  // variáveis
+for(var n2 = n ;n2 <=10 ;n2+=2){   //loppings ou laços de repetição
+    console.log(`${n2}`)
+    if( n2 == 2){  // condicional
+        console.log(`Esse é o número 2 !`)
+    } else{
+        console.log(`Esse não é o número que procuro!!`)
+    }
+}
+
+function somar(){ // funções
+    var n3 = window.document.getElementById('teste')
+}
+
+
+// esses são os pontos de js que ja estão bem gravados na minha cabeça!!!
