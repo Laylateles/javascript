c766029b643084833f1e1e267d95224ed2c36bfa
Laylateles <135770455+Laylateles@users.noreply.github.com>
2023-10-04 20:53:42 -0300

    fazendo novo exercicio

--- estudos/traversy/exlivro/03.js
+++ estudos/traversy/exlivro/03.js
@@ -3,12 +3,12 @@ for(var n1 = 1;n1 <= 100;n1+=1){
         console.log('Fizz')
     } else if (n1 % 5 == 0){
         console.log('Buzz')
-    }
-    // } else if (n1 % 5 == 0 || n1 % 3 == 0){
-    //     console.log('teste')
-    // }
-     else{
+    }else{
         console.log(`${n1}`)
     }
+      if(n1 % 5 == 0 && n1 % 3 == 0){
+        console.log('FizzBuzz')
+    } //teve q ser feito por fora pois não faz parte da condicional anterior
 }
-// termino amanhã,
+
+// segundo exercicio concluido!
---
+++ estudos/traversy/exlivro/04.js
@@ -0,0 +1,5 @@
+var tab = '#'
+while(tab <='####'){
+    console.log(`${tab}`)
+    tab+='#'
+}
