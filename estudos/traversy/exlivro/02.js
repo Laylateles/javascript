e73c73c07d51e910bf8eda5b1241c9537bc4f42a
Laylateles <135770455+Laylateles@users.noreply.github.com>
2023-10-03 21:42:15 -0300

    segundo exercicio do livro

---
+++ estudos/traversy/exlivro/02.js
@@ -0,0 +1,7 @@
+var n = '#'
+while(n <= '#######'){
+    console.log(`${n}`)
+    n += '#'
+}
+// muito mais dificil doq parece primeiro exercicio feito do livro!!
+
---
+++ estudos/traversy/exlivro/03.js
@@ -0,0 +1,14 @@
+for(var n1 = 1;n1 <= 100;n1+=1){
+     if(n1 % 3 == 0){
+        console.log('Fizz')
+    } else if (n1 % 5 == 0){
+        console.log('Buzz')
+    }
+    // } else if (n1 % 5 == 0 || n1 % 3 == 0){
+    //     console.log('teste')
+    // }
+     else{
+        console.log(`${n1}`)
+    }
+}
+// termino amanhã,
