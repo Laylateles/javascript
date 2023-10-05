94cb708962f5325990aa438b22f3f975f6591e9f
Laylateles <135770455+Laylateles@users.noreply.github.com>
2023-10-05 20:49:01 -0300

    update04.js

--- estudos/traversy/exlivro/04.js
+++ estudos/traversy/exlivro/04.js
@@ -1,6 +1,7 @@
-    for(var rep =' # ' + ' # ' + ' # ' + ' #\n' ;rep <= '##';rep += '#'){
+    for(var rep =' # ' + ' # ' + ' # ' + ' #\n' ;rep < '#' ;rep += '#'){
         console.log(rep)
-    }
+        break
+    } //não tentar rodar esse código,se eu n reescrever outra coisa aq é pq vscode travou dnv!!!!!
 
 
 // console.log(' # ' + ' # ' + ' # ' + ' #\n') //repetição
