c749c71fc919a7a994ad30ea8b9b4d8bb4928647
Laylateles <135770455+Laylateles@users.noreply.github.com>
2023-10-17 20:12:41 -0300

    ex livro

---
+++ estudos/traversy/exlivro/07.js
@@ -0,0 +1,33 @@
+function range(ini,fim){
+    if(ini == undefined || fim == undefined){
+       return 1
+    } else{
+        let cont = []
+        
+    }
+    return cont
+}
+
+
+console.log(([1,2,5,10]).slice(0,5))
+/*
+-->se inicializa com um let=[]
+-->método push para adicionar valores
+-->retornar o array no final da função
+-->vai ter um looping
+-->vai ter dois loopings diferentes,um para contar valores crescentes e outro decrescente
+-->@@@@@ retorna um array contendo todos os valores apartir de ini,até o valor fim(concluindo)###
+-->ex bônus,modificar essa função para aceitar um terceiro argumento,que indica o tamanho do incremento usado para construir o array.
+
+*/
+
+function sum(num){
+
+}
+
+/*
+-->ele ai receber um array de números como argumento,e retorna a soma deles
+-->
+-->
+
+*/
