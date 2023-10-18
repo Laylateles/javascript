3b434665e8a3ab9ed8fbfd3bc8b7b26d4bef4e43
Laylateles <135770455+Laylateles@users.noreply.github.com>
2023-10-17 21:55:03 -0300

    ex livro

--- estudos/traversy/exlivro/07.js
+++ estudos/traversy/exlivro/07.js
@@ -1,22 +1,33 @@
 function range(ini,fim){
     if(ini == undefined || fim == undefined){
        return 1
-    } else{
-        let cont = []
-        
+    } else {
+        let cont =[]
+        for(var a = ini; a <= fim; a++){
+         cont.push(a)
+         
+        } if(ini > fim){
+            for(var b = ini ; b >= fim; b--){
+                cont.push(b)
+            }
+        }
+        return cont
     }
-    return cont
 }
+var res = range(10,3)
+console.log(`${res}`)
 
+//terminado a primeira parte !!!!!!
 
-console.log(([1,2,5,10]).slice(0,5))
 /*
--->se inicializa com um let=[]
--->método push para adicionar valores
--->retornar o array no final da função
--->vai ter um looping
--->vai ter dois loopings diferentes,um para contar valores crescentes e outro decrescente
--->@@@@@ retorna um array contendo todos os valores apartir de ini,até o valor fim(concluindo)###
+se inicializa com um let=[] xxx
+método push para adicionar valores xxx
+retornar o array no final da função xxx
+vai ter um looping xxx
+vai ter dois loopings diferentes,um para contar valores crescentes e outro decrescente xxx
+retorna um array contendo todos os valores apartir de ini,até o valor fim(concluindo) xxx
+----------------------------------------------------------------------------------------------------
+
 -->ex bônus,modificar essa função para aceitar um terceiro argumento,que indica o tamanho do incremento usado para construir o array.
 
 */
