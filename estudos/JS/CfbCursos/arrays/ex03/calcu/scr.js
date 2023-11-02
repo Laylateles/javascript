477215de3661ba014c359106daf17f29e284deb0
Laylateles <135770455+Laylateles@users.noreply.github.com>
2023-11-02 20:31:48 -0300

    mudando as pastas

--- estudos/JS/CfbCursos/arrays/ex03/calcu/scr.js
+++ estudos/JS/CfbCursos/arrays/ex03/calcu/scr.js
@@ -10,73 +10,60 @@ const limpar = document.querySelector('#limpar')
 let valores =[n1,n2]//não foi convertido aq pq resultaria no valor de uma string
 
 const op =[
-    (val)=>{
-        let res = 0
-         for(let v of val){
-             res+= Number(v.value)//ele percorre o input e converte as strings para números
-         }
-         return res
+    ()=>{
+        return Number(n1.value) + Number(n2.value)
         },
     ()=>{
-       let res =null //não sei por onde iniciae
-       for(v of valores){
-        res -= Number(v.value)
-       }
-       return res
+        resultado.innerHTML += Number(n1.value) - Number(n2.value)
+       },
+    ()=>{
+        resultado.innerHTML += Number(n1.value) * Number(n2.value)
     },
     ()=>{
-        let res = 1
-         for(let v of valores){
-             res *= Number(v.value)//ele percorre o input e converte as strings para números
-        }
-         return res
-    },
-    (val)=>{
-        let res = null  //não sei por onde iniciae
-         for(let v of val){
-            res/= Number(v.value)//ele percorre o input e converte as strings para números
-        }
-            return res
+        resultado.innerHTML += Number(n1.value) / Number(n2.value)
     }
 ]
 soma.addEventListener('click',()=>{
     let resu = op[0](valores)
-    resultado.innerHTML += resu
+    resultado.innerHTML += resu //precisa do inner esse para mostrar o resu
 })
 sub.addEventListener('click',()=>{
-    let resu = op[1](valores)
-    resultado.innerHTML += resu
+    op[1](valores) //não precisa criar um let para chamar
 })
-multi.addEventListener('click',()=>{
-    let resu = op[2](valores)
-    resultado.innerHTML += resu
-})
+multi.addEventListener('click',op[2])//vai chamar o array da posição 2 quando clicar
 divi.addEventListener('click',()=>{
-    let resu = op[3](valores)
-    resultado.innerHTML += resu
+    op[3](valores)
 })
-
-
 limpar.addEventListener('click',()=>{
     resultado.innerHTML =`Resultado:` + ''
 })
 
-//calcu simples,dois campos de texto
-//um campo para entrada dos valores
-//executar aas funções apartir desse valores
-//vai ter o botão de :soma,sub,multi,divi
-//adicionar as funções da calculadora dentro d um array 
-// soma.addEventListener('click',(val)=>{//o valo n pode ser o msm val do outro script pq esse val vai estar recebendo o elemento
-/*
-pq não convertemos o n1 e n2 = n1 e n2 já são referências a elementos HTML (<input>), e ao acessar a propriedade value deles, obtemos o valor como uma string.
-
-*/
-
 
 /*
- let res = null // não consegui pensar por qual número o res pode inicializar
-         for(let v of valores){
-           res -= Number(v.value)//ele percorre o input e converte as strings para números
-        }
-        return res
+const op =[
+    (val)=>{
+        return Number(n1.value) + Number(n2.value)
+        //resultado.innerHTML += Number(n1.value) + Number(n2.value)
+        //3 maneiras de fazer esse código
+        // let res = 0
+        //  for(let v of val){
+        //      res+= Number(v.value)//ele percorre o input e converte as strings para números
+        //  }
+        //  return res
+        },
+    ()=>{
+        resultado.innerHTML += Number(n1.value) - Number(n2.value)
+       },
+    ()=>{
+        resultado.innerHTML += Number(n1.value) * Number(n2.value)
+        // let res = 1
+        //  for(let v of valores){
+        //      res *= Number(v.value)//ele percorre o input e converte as strings para números
+        // }
+        //  return res
+    },
+    (val)=>{
+        resultado.innerHTML += Number(n1.value) / Number(n2.value)
+    }
+]
 */
