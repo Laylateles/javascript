0ee8e9b2ff6a23de4d28f907359026233ba7d2a3
Laylateles <135770455+Laylateles@users.noreply.github.com>
2023-11-11 17:01:21 -0300

    exe

---
+++ estudos/JS/CfbCursos/objetos/obj.07/src.js
@@ -0,0 +1,7 @@
+const nome = document.querySelector('#nome')
+const porta = document.querySelector('#num1')
+const blindagem = document.querySelector('#num2')
+const municao = document.querySelector('#num3')
+const radios = document.querySelector('')
+
+// terminar na segunda
