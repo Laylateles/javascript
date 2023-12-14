0fb2865f1a526e153dd84e7bd571944382a3e515
Laylateles <135770455+Laylateles@users.noreply.github.com>
2023-12-13 22:19:23 -0300

    math

---
+++ estudos/JS/CfbCursos/math/ex1/math.js
@@ -0,0 +1,13 @@
+//não precisamos instanciar o obj da class math
+//const mat = new Math()
+
+
+Math.PI//valor de pi
+Math.random()//gera número aleatório entre 0 e 1
+//se quiser que ele esteja entre um número maior multiplique pelo número máximo que vc queira,Math.random()*2
+Math.floor()//arredonda para o número mais próximo
+Math.floor(Math.random()*11)//gera apenas numeros aleatórios inteiros
+Math.round()//tambem arredonda
+Math.sqrt()//retorna a raiz quadrada
+Math.pow(10,3)//aqui você passa o número e o valor que ele vai ser elevado,10 elevado ao cubo
+console.log(Math.pow(5,2))
